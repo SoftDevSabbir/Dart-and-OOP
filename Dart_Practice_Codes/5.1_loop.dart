f1bdb2b8914e8be614117156b05f2a9e871dc33a
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-08-22 15:26:35 +0600

    5.1_loop.dart

--- Dart_Practice_Codes/5.1_loop.dart
+++ Dart_Practice_Codes/5.1_loop.dart
@@ -1,1 +1,22 @@
+// ## Loop in dart
+void main(){
+  //# print 1 to 5
+  for (var i=1;i<=5;i++){
+    print("Number :$i");
+  }
+ // # print 6 to 10
+  int i=6;
+  while(i<=10){
+    print(i);
+    i++;
+  }
 
+  //# print 10 to 15
+  int i1=11;
+  do{
+    print(i1);
+    i1++;
+  }while(i1<=15);
+
+
+ }
