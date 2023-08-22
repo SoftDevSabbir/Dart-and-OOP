8d12f0b0f8b492479c81fcb3cfb7f615ceb5b45d
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-08-22 21:49:41 +0600

    7.0__function.dart

--- Dart_Practice_Codes/7.0__function.dart
+++ Dart_Practice_Codes/7.0__function.dart
@@ -1,74 +1,27 @@
 void main() {
+  add();
+  add2(3, 4);
+  // ignore: unused_local_variable
+  int ans = add3(3, 4, 5);
+  print(ans);
+  print("\n");
+}
 
-  ///## All Type of Function
-  //# function without parameter
-  void myfun() {
-    print("Hello Dart");
-  }
-  myfun();
+void add() {
+  print("I am add function for adding numbers : ");
+  print(5 + 5);
+  print("\n");
+}
 
-  int addfun() {
-    int a = 10;
-    int b = 20;
-    return a + b;
-  }
-  var total = addfun();
-  print(total);
+void add2(int a, int b) {
+  print("I am add function 2, for adding given parameterized numbers sum : ");
+  print(a + b);
+  print("\n");
+}
 
-  // ### Function with parameter
-  // ## Positional Parameter
-  int multifun(a, b) {
-    return a * b;
-  }
-  var sum = multifun(10, 20);
-  print(sum);
-
-
-  // String introfun(String name, String City){
-  //   return 'I am $name from $City';
-
-  /// ## Optional Positional Parameter
-  //## Null-abble type parameter
-
-  /*  String introfun(String name, [String? City]){
-    return 'I am $name from $City';
-  }
- var info= introfun("Sabbir");
-  print(info); */
-
-  //useing logic
-  String introfun(String name, [String? City]) {
-    if (City != null) {
-      return 'I am $name from $City';
-    }
-    return 'I am $name';
-  }
-  var info = introfun("Sabbir");
-  print(info);
-  // ## Default value parameter
-
-  String address(String name, [String City = 'Kushtia']) {
-    return 'I am $name from $City';
-  }
-  var add = address("SABBIR ISLAM");
-  print(add);
-
-  // ## Named Parameter
-  sumfun(a, b) {
-    print('A:$a');
-    print('B:$b');
-  }
-   sumfun(50, 100);
-
-  //## Anonymous Function
-
-  var student=['Sabbir','Likhon','Noble'];
-  student.forEach((value) {print(value); });
-
-  print(" ");
-
-  //## Arrow Function
-  var student1=['Jaber','Asik','Monir'];
-  student1.forEach((value)=>print(value) );
-  //print(student1);
+//  return function
+int add3(int a, int b, int c) {
+  print("I am returning function to return the sum of given parameter : ");
+  int sum = a + b + c;
+  return sum; // returning
 }
