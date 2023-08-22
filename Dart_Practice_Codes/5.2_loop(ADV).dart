b2dee88b8e83a0fc692a24c7ae52aa1006b56707
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-08-22 16:41:01 +0600

    5.2_loop(ADV).dart

---
+++ Dart_Practice_Codes/5.2_loop(ADV).dart
@@ -0,0 +1,57 @@
+import 'dart:html';
+import 'dart:js_util';
+
+void main(){
+  ///### Loop in dart
+  ///
+//# for loop
+  List <String>  students=['Sabbir','Asif','Likhon','Nirob'];
+  for (var i=0;i<students.length; i++){
+    print(students[i]);
+  }
+
+  print(" ");//for space create
+
+  //## for-in loop with List
+  List <String>  student=["Sabbir","Ahamed","Eshan","Asraful"];
+  for (var stu in student){
+    print(stu);
+  }
+  print(" ");//for space create
+
+  //## for-in loop with set
+  Set <String>  student20={"CokaCola","FastFood","Meet","Vesitale"};
+  for (var stu1 in student20){
+    print(stu1);
+  }
+
+  print(" ");//for space create
+
+  //## for-in loop with Map
+  Map <String,String>  Profile= {'name': 'Esan Ahamed Ahad','City':'Kushtia','State':'Khulna'};
+  for (var key in Profile.keys){
+    print(key);
+  }
+  print(" ");//for space create
+
+  for (var value in Profile.values){
+   print(value);
+  }
+
+  ///## forEach loop
+  //# forEach loop with List (List and Set same procedure)
+
+ List <String> name=['Sonam','Abir','Sumit','RAbbil'];
+  name.forEach((x)=>print (x));
+  
+  //# forEach loop useing List to Map convert
+    name.asMap().forEach((i, value)=> ('$i=$value'));
+
+  //# forEach loop with Map
+
+  Map<String,String> address={
+    'Name':'Sabbir','City':'Kushtia','State':'khulna'
+  };
+  address.forEach((x, y)=>print('$x=$y'));
+
+}
