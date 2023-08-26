a1b841305304f03f32cfc5126fc3a0c046f3e1a0
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-08-26 22:26:27 +0600

    11.0_OOP_this.dart

--- Dart-OOP (codes)/11.0_OOP_this.dart
+++ Dart-OOP (codes)/11.0_OOP_this.dart
@@ -1,1 +1,35 @@
 
+
+/// this keyword
+
+class Student {
+  String name='';
+  int age= 0;
+  //
+  // //constractor
+  // Student (String n, int a){
+  //   name=n;
+  //   age=a;
+  // }
+
+  // Student (String name, int age){
+  //   name=name;
+  //   age=age;
+  // }
+
+  Student (String name , int age ){
+    this.name=name;
+    this.age=age;
+  }
+
+  //method
+  void displayInformation(){
+    print(name);
+    print(age);
+  }
+}
+
+void main(){
+  Student obj =Student("Sabbir", 18);
+  obj.displayInformation();
+}
