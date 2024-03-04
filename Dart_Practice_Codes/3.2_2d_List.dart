0fcd86906b54f7df72e9c2221f4265937b1ddfdb
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2024-03-04 21:06:35 +0600

    3.2_2d_List.dart

---
+++ Dart_Practice_Codes/3.2_2d_List.dart
@@ -0,0 +1,36 @@
+void main() {
+  List<List<String>> myList = [
+    ["A", "B", "C", "D", "E", "F"],
+    ["am", 'is', 'are', 'was', 'were'],
+  ];
+  print(myList);
+  print(myList[1][3]);
+
+  List<List<int>> numList = [
+    [1, 2, 3, 4, 5, 6, 7, 8],
+    [12, 34, 56, 66],
+    [234, 45, 6767, 788]
+  ];
+  print(numList);
+  print(numList[2][2]);
+
+  List<List<dynamic>> bList = [
+    [true, false],
+    [12, 3456, 45],
+    ["Hi", "Hello"]
+  ];
+  print(bList[0][1]);
+
+  List<List<List<dynamic>>> newList = [
+    [
+      [1, "c"],
+      [6, true]
+    ],
+    [
+      [1, ""],
+      ["a", 5]
+    ]
+  ];
+  print(newList);
+  print(newList[1][0][0]);
+}
